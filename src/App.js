6595ccc4fea050a9f0bd850111dc1c2065bbe6db
hohnghia <hieunghia1909@gmail.com>
2021-06-19 15:37:03 +0700

    tạo conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
-
+      <ColorBox />
     </div>
   );
 }
